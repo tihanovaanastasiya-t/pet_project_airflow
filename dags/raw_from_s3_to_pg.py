f00e67be967a38b5459bac9eb2967595f577fa1c
Tihanova Anastasiya <tihanovaanastasiya@gmail.com>
2026-03-15 18:35:09 +0100

    raw_from_s3_to_pg configuration2

--- dags/raw_from_s3_to_pg.py
+++ dags/raw_from_s3_to_pg.py
@@ -33,7 +33,7 @@ SHORT_DESCRIPTION = "SHORT DESCRIPTION"
 
 args = {
     "owner": OWNER,
-    "start_date": pendulum.datetime(2025, 5, 1, tz="Europe/Moscow"),
+    "start_date": pendulum.datetime(2026, 3, 8, tz="Europe/Moscow"),
     "catchup": True,
     "retries": 3,
     "retry_delay": pendulum.duration(hours=1),
